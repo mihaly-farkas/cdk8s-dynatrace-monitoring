ccde8886069c690689562f3f5d7a494089ab5591
Mihaly Farkas <194117456+mihaly-farkas@users.noreply.github.com>
2025-04-02 22:56:56 +0200

    feat: implement basic platform monitoring...
    Implement the basic installation scenario described in the Dynatrace documentation.
    
    See: https://docs.dynatrace.com/docs/ingest-from/setup-on-k8s/deployment/platform-observability
    
    --------------
    
    Namespace handling implemented.

--- src/dynatrace-kubernetes-monitoring.ts
+++ src/dynatrace-kubernetes-monitoring.ts
@@ -1,9 +1,40 @@
 import { Construct } from 'constructs';
+import { Namespace } from 'cdk8s-plus-32';
+import { ApiObjectMetadata } from 'cdk8s';
+
+const DEFAULT_NAMESPACE = 'dynatrace';
 
 /**
  * Properties for configuring Dynatrace Kubernetes monitoring.
  */
 export interface DynatraceKubernetesMonitoringProps {
+
+  /**
+   * The name of the namespace.
+   *
+   * If not specified, the `dynatrace` name will be used.
+   *
+   * @default - The `dynatrace` namespace will be used.
+   */
+  readonly namespace?: string;
+
+  /**
+   * Properties for the namespace to be created.
+   *
+   * If {@link skipNamespaceCreation} is set to `true`, this property will be ignored.
+   */
+  readonly namespaceProps?: Omit<ApiObjectMetadata, 'name' | 'namespace'>;
+
+
+  /**
+   * Whether to skip the creation of the Dynatrace monitoring namespace.
+   *
+   * If set to `true`, the namespace will not be created automatically, and we assume that the user has already created
+   * it.
+   *
+   * @default false
+   */
+  readonly skipNamespaceCreation?: boolean;
 }
 
 /**
@@ -13,17 +44,51 @@ export interface DynatraceKubernetesMonitoringProps {
  */
 export class DynatraceKubernetesMonitoring extends Construct {
 
+  public readonly namespace?: Namespace;
+
+  private readonly props: DynatraceKubernetesMonitoringProps;
+
   /**
    * Instantiates a new Dynatrace Kubernetes monitoring instance.
    *
    * @param scope The scope in which this construct is defined.
    * @param id The id of the construct.
-   * @param props
+   * @param props The properties of the construct.
    */
   constructor(scope: Construct, id: string, props: DynatraceKubernetesMonitoringProps = {}) {
     super(scope, id);
 
-    // TODO: Remove this. It is only here to prevent the "unused variable" error.
-    console.log(props);
+    this.props = props;
+
+    this.namespace = this.createNamespace(props);
+  }
+
+  public get namespaceName() {
+    return this.namespace?.name ?? this.props.namespace ?? DEFAULT_NAMESPACE;
+  }
+
+  /**
+   * Creates the namespace for Dynatrace monitoring if required.
+   *
+   * If {@link skipNamespaceCreation} is set to `false` or not provided, the namespace is created with the default name
+   * (see: {@link DEFAULT_NAMESPACE}).
+   * If {@link skipNamespaceCreation} is `true`, no namespace is created.
+   *
+   * @param props - The properties of the construct.
+   * @returns The created Namespace, or `undefined` if namespace creation is skipped.
+   */
+  private createNamespace(props: DynatraceKubernetesMonitoringProps): Namespace | undefined {
+    let namespace = undefined;
+
+    if (props.skipNamespaceCreation !== true) {
+      namespace = new Namespace(this, 'Namespace', {
+        metadata: {
+          name: this.namespaceName,
+          ...props.namespaceProps,
+        },
+      });
+    }
+
+    return namespace;
   }
 }
--- test/dynatrace-kubernetes-monitoring.test.ts
+++ test/dynatrace-kubernetes-monitoring.test.ts
@@ -9,9 +9,82 @@ describe('a Dynatrace Kubernetes monitoring instance', () => {
     const chart = new Chart(app, 'test');
 
     // WHEN
-    new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring');
+    const monitoring = new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring');
 
     // THEN
+    expect(monitoring.namespace).toBeDefined();
+    expect(monitoring.namespaceName).toBe('dynatrace');
+    expect(Testing.synth(chart)).toMatchSnapshot();
+  });
+
+  test('with custom namespace name', () => {
+    // GIVEN
+    const app = Testing.app();
+    const chart = new Chart(app, 'test');
+
+    // WHEN
+    const monitoring = new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring', {
+      namespace: 'custom-namespace',
+    });
+
+    // THEN
+    expect(monitoring.namespace).toBeDefined();
+    expect(monitoring.namespaceName).toBe('custom-namespace');
+    expect(Testing.synth(chart)).toMatchSnapshot();
+  });
+
+  test('with custom namespace props', () => {
+    // GIVEN
+    const app = Testing.app();
+    const chart = new Chart(app, 'test');
+
+    // WHEN
+    const monitoring = new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring', {
+      namespaceProps: {
+        annotations: {
+          'custom-annotation': 'value',
+        },
+        labels: {
+          'custom-label': 'value',
+        },
+      },
+    });
+
+    // THEN
+    expect(monitoring.namespace).toBeDefined();
+    expect(monitoring.namespaceName).toBe('dynatrace');
+    expect(Testing.synth(chart)).toMatchSnapshot();
+  });
+
+  test('with skip namespace creation', () => {
+    // GIVEN
+    const app = Testing.app();
+    const chart = new Chart(app, 'test');
+
+    // WHEN
+    const monitoring = new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring', {
+      skipNamespaceCreation: true,
+    });
+
+    // THEN
+    expect(monitoring.namespace).toBeUndefined();
+    expect(monitoring.namespaceName).toBe('dynatrace');
+    expect(Testing.synth(chart)).toMatchSnapshot();
+  });
+
+  test('with skip namespace creation and custom namespace name', () => {
+    const app = Testing.app();
+    const chart = new Chart(app, 'test');
+
+    // WHEN
+    const monitoring = new DynatraceKubernetesMonitoring(chart, 'dynatrace-monitoring', {
+      namespace: 'custom-namespace',
+      skipNamespaceCreation: true,
+    });
+
+    // THEN
+    expect(monitoring.namespace).toBeUndefined();
+    expect(monitoring.namespaceName).toBe('custom-namespace');
     expect(Testing.synth(chart)).toMatchSnapshot();
   });
 });
